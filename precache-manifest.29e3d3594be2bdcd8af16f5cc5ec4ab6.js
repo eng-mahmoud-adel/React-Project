aa80d050c56de33ce964583450f1ba60ad7cf578
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-01-06 00:21:36 +0200

    Updates

---
+++ precache-manifest.29e3d3594be2bdcd8af16f5cc5ec4ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d82a347046955b6e4bbd52dd7301c5",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "7d7f5fcc434f947de487",
+    "url": "/React-Project/static/css/main.c39b838f.chunk.css"
+  },
+  {
+    "revision": "4ffbafcf4eb4beef6059",
+    "url": "/React-Project/static/js/2.6051edc3.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/React-Project/static/js/2.6051edc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d7f5fcc434f947de487",
+    "url": "/React-Project/static/js/main.4b8ff456.chunk.js"
+  },
+  {
+    "revision": "df33e3063583b635b43e",
+    "url": "/React-Project/static/js/runtime-main.f3d334e1.js"
+  }
+]);
